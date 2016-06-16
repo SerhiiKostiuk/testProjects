f61fb0593f65f1f57259bf1519a7cf794f286d16
Serhii Kostiuk <k.seryoga@gmail.com>
2016-06-16 10:48:22 +0300

    add KSHeaderTableViewCell

---
+++ Source/Views/KSPrepsDetailTableViewCell/KSPrepsDetailTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  KSPrepsDetailTableViewCell.h
+//  adamaTestProject
+//
+//  Created by Сергій Костюк on 15.06.16.
+//  Copyright © 2016 Сергій Костюк. All rights reserved.
+//
+
+#import "KSPrepsTableViewCell.h"
+
+@interface KSPrepsDetailTableViewCell : KSPrepsTableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *prepInfoTextLabel;
+
+@end
---
+++ Source/Views/KSPrepsDetailTableViewCell/KSPrepsDetailTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  KSPrepsDetailTableViewCell.m
+//  adamaTestProject
+//
+//  Created by Сергій Костюк on 15.06.16.
+//  Copyright © 2016 Сергій Костюк. All rights reserved.
+//
+
+#import "KSPrepsDetailTableViewCell.h"
+
+@implementation KSPrepsDetailTableViewCell
+
+@end
