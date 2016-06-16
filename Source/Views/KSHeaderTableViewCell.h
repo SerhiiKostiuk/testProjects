54859d09f5b945822bd2e1f80bc6653dba7f3ba1
Serhii Kostiuk <k.seryoga@gmail.com>
2016-06-16 10:50:08 +0300

    add cell for header image

---
+++ Source/Views/KSHeaderTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  KSHeaderTableViewCell.h
+//  adamaTestProject
+//
+//  Created by Сергій Костюк on 16.06.16.
+//  Copyright © 2016 Сергій Костюк. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KSHeaderTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *headerImageView;
+
+
+
+@end
---
+++ Source/Views/KSHeaderTableViewCell.m
@@ -0,0 +1,14 @@
+//
+//  KSHeaderTableViewCell.m
+//  adamaTestProject
+//
+//  Created by Сергій Костюк on 16.06.16.
+//  Copyright © 2016 Сергій Костюк. All rights reserved.
+//
+
+#import "KSHeaderTableViewCell.h"
+
+@implementation KSHeaderTableViewCell
+
+
+@end
